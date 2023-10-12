1ab5ae848a343c3242df48f66b4d9d7e013b1843
OnlyYellow <ash1823@naver.com>
2023-10-12 14:19:50 +0900

    fix/accounts_models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,13 @@
-from django.db import models, transaction
+from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.hashers import make_password
 
 
-# Create your models here.
 # 헬퍼 클래스 - 유저를 생성할 때 사용
-class UserManager(BaseUserManager):
-    use_in_migrations = True
-    
-    def create_user(self, account_id, user_name, password, **extra_fields):
+class UserManager(BaseUserManager):    
+    def create_user(self, account_id, user_name, password, **kwargs):
         """
         주어진 id, name(별명), 비밀번호 개인정보로 User 인스턴스 생성
         """        
@@ -24,27 +21,25 @@ class UserManager(BaseUserManager):
         user = self.model(
             account_id=account_id,
             user_name=user_name,
-            **extra_fields
         )
-        hashed_password = make_password(password)
-        user.set_password(hashed_password)
+        user.set_password(password)
         user.save(using=self._db)
-        
         return user
     
-    def create_superuser(self, account_id, user_name, password):
+    def create_superuser(self, account_id=None, user_name=None, password=None, **extra_fields):
         """
         주어진 id, name(별명), 비밀번호 개인정보로 User 인스턴스 생성
         단, 최상위 사용자이므로 권한을 부여
         """
-        superuser = self.model(
+        superuser = self.create_user(
             account_id=account_id,
             user_name=user_name,
+            password=password,
         )
-        superuser.set_password(password)
         superuser.is_active = True # 해당 계정이 활성인지를 결정
         superuser.is_staff = True # 참인 경우 admin 사이트에 접속 가능
         superuser.is_superuser = True # 모든 권한 부여
+        
         superuser.save(using=self._db)
         return superuser
     
@@ -53,7 +48,8 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):    
     account_id = models.CharField(max_length=30, unique=True, null=False, blank=False)
     user_name = models.CharField(max_length=30, null=False, blank=False)
-    date_joined = models.DateTimeField(_('created_at'), default=timezone.now)
+    date_joined = models.DateTimeField(_('created_at'), auto_now_add=True)
+    date_updated = models.DateTimeField(_('updated_at'), auto_now=True)
     is_active = models.BooleanField(_('active_status'), default=True)
     is_staff = models.BooleanField(_('staff_status'), default=False)
     is_superuser = models.BooleanField(_('superuser_status'), default=False)
@@ -63,6 +59,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     # 헬퍼 클래스 사용
     objects = UserManager()
-    
-    def __str__(self):
-        return self.account_id
