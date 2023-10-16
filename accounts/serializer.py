47f58f8b80f4ff999e80b9859cccb1179d2fcdf6
OnlyYellow <ash1823@naver.com>
2023-10-16 19:06:40 +0900

    fix/account serializer.py_add other info

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -11,6 +11,10 @@ class UserSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             account_id = validated_data['account_id'],
             user_name = validated_data['user_name'],
+            tiiun_number=validated_data['tiiun_number'],
+            cultivation_experience=validated_data['cultivation_experience'],
+            garden_size=validated_data['garden_size'],
+            address=validated_data['address'],
             password = validated_data['password']
         )
         return user
