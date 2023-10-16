8f56e4d4bd6ce71318a351a69f4db346c24ebd3d
OnlyYellow <ash1823@naver.com>
2023-10-16 21:02:28 +0900

    fix/accounts admin.py_see new info which was typed in models.py when login to admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,7 @@ from accounts.models import User
 
 # Register your models here.
 class UserCheck(UserAdmin):
-    list_display = ('account_id', 'user_name', 'date_joined', 'is_staff', 'is_active')
+    list_display = ('account_id', 'user_name', 'tiiun_number', 'garden_size', 'address', 'date_joined', 'date_updated', 'is_staff', 'is_active')
     list_filter = ('is_active', 'is_staff', 'is_superuser', 'date_joined')
     search_fields = ('account_id', 'user_name')
     ordering = ('-date_joined',)
@@ -13,14 +13,14 @@ class UserCheck(UserAdmin):
     # admin 페이지에서 사용자 수정할 때 입력폼
     fieldsets = (
         ('user', {'fields': ('password',)}),
-        ('Personal Info', {'fields': ('user_name',)}),
+        ('Personal Info', {'fields': ('user_name', 'tiiun_number', 'garden_size', 'address')}),
     )
     
     # admin 페이지에서 사용자 추가할 때 입력폼
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('account_id', 'user_name', 'password1', 'password2')
+            'fields': ('account_id', 'user_name', 'tiiun_number', 'garden_size', 'address', 'password1', 'password2')
         }),
     )
     
