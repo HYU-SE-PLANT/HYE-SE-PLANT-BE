ff6f8342f946e911d6105672f9952c99582b3c89
OnlyYellow <ash1823@naver.com>
2023-10-26 21:29:59 +0900

    feat/community model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+from accounts.models import User
 
-# Create your models here.
+
+class Community(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False) # 게시글의 id 값
+    question_title = models.CharField(max_length=255, null=False, blank=False) # 제목 길이: 최대 255자
+    question_content = models.TextField(null=False, blank=False) # 내용 입력
+    question_date = models.DateTimeField(auto_now_add=True) # 작성 날짜
+    answer_or_not = models.BooleanField(default=False) # 답변 여부
+    questioner_id = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE) # 작성자
