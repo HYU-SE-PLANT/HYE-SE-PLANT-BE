26760a4eed2018ff3e9bfd4ebfc83f1f15a348d4
OnlyYellow <ash1823@naver.com>
2023-10-26 23:25:11 +0900

    feat/ admin.py 어드민 페이지에서 질의응답 목록 보기

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from community.models import Community
+
 
 # Register your models here.
+class CommunityCheck(admin.ModelAdmin):
+    list_display = ('id', 'user' , 'question_title', 'question_date', 'question_updated_date', 'answer_or_not')
+    search_fields = ('user', 'question_title')
+    ordering = ('-question_date',)
+    
+    fieldsets = (
+        ('question_detail', {'fields': ('question_title', 'question_content')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('question_title', 'question_content')
+        }),
+    )
+
+admin.site.register(Community, CommunityCheck)
