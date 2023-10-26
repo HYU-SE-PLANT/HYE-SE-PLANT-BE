2c4485fff5cdfc9ec6fbd16f512b5f69b2e00e27
OnlyYellow <ash1823@naver.com>
2023-10-26 21:50:51 +0900

    feat/community view

--- community/views.py
+++ community/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Community
+from .serializer import CommunitySerializer
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.views import APIView
+
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+# Community에 올라온 질문 목록 보여주기
+class QuestionList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
+    # 질의응답 리스트 보여줄 때
+    def get(self, request):
+        questions = Community.objects.all()
+        serializer = CommunitySerializer(questions, many=True)
+        return Response(serializer.data)
