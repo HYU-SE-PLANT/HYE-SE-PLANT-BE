570700fd57dc21eec749e0c0189ad966a0c6d1a4
OnlyYellow <ash1823@naver.com>
2023-10-26 22:43:13 +0900

    feat/ community see detail of question

--- community/urls.py
+++ community/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', QuestionList.as_view()),
     path('create/', QuestionCreate.as_view()),
+    path('<int:pk>/', QuestionDetail.as_view()),
 ]
--- community/views.py
+++ community/views.py
@@ -4,6 +4,7 @@ from .pagination import CustomResultsSetPagination
 
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 from rest_framework.views import APIView
 
@@ -37,3 +38,22 @@ class QuestionCreate(APIView):
             serializer.save(user = request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+# 질문 세부사항 관련
+class QuestionDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
+    # 객체 가져오기
+    def get_object(self, pk):
+        try:
+            return Community.objects.get(pk=pk)
+        except Community.DoesNotExist:
+            raise Http404
+        
+    # 질문 상세히 보기
+    def get(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = CommunitySerializer(question)
+        return Response(serializer.data)
