4e0765222cf0020e57461f61d8e5521d31e94142
OnlyYellow <ash1823@naver.com>
2023-10-27 16:07:15 +0900

    fix/question serializer add answer or not

--- community/serializer.py
+++ community/serializer.py
@@ -8,7 +8,7 @@ class CommunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
         # 표시되는 항목
-        fields = ['id', 'question_title', 'question_date', 'question_updated_date', 'user', 'question_content']
+        fields = ['id', 'question_title', 'question_date', 'question_updated_date', 'user', 'question_content', 'answer_or_not']
         
 
 class CommentSerializer(serializers.ModelSerializer):
