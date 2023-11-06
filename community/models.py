94b023f03b9d1c6a965d8d262cf749c089726b02
A_Snow <ash1823@naver.com>
2023-11-06 22:57:29 +0900

    fix/ 원래대로 복구

--- community/models.py
+++ community/models.py
@@ -6,7 +6,7 @@ from accounts.models import User
 class Community(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False) # 게시글의 id 값
     question_title = models.CharField(max_length=255, null=False, blank=False) # 제목 길이: 최대 255자
-    created_at = models.TextField(null=False, blank=False) # 내용 입력
+    question_content = models.TextField(null=False, blank=False) # 내용 입력
     created_at = models.DateTimeField(auto_now_add=True) # 작성 날짜
     user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE) # 작성자
     
