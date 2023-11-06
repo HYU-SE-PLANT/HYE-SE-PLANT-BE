d8e07d67cf7e21d2a0bcc1f23ea5664a5051967d
OnlyYellow <ash1823@naver.com>
2023-11-07 00:09:34 +0900

    fix/ community admin page error 수정

--- community/admin.py
+++ community/admin.py
@@ -7,16 +7,5 @@ class CommunityCheck(admin.ModelAdmin):
     list_display = ('id', 'user' , 'question_title', 'created_at')
     search_fields = ('user', 'question_title')
     ordering = ('-created_at',)
-    
-    fieldsets = (
-        ('question_detail', {'fields': ('question_title', 'question_content')}),
-    )
-    
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('question_title', 'question_content')
-        }),
-    )
 
 admin.site.register(Community, CommunityCheck)
