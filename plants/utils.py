148bcf93e4593cd9941a757b66c3fde983a6110c
OnlyYellow <ash1823@naver.com>
2023-11-08 01:11:32 +0900

    feat/ views.py see all the list of plant

--- plants/utils.py
+++ plants/utils.py
@@ -1,6 +1,29 @@
+from django.utils import timezone
+
+
 # is_harvested 를 결정하는 함수
 def determine_is_harvested(harvested_at):
     """
     날짜가 제공되면 True, 그렇지 않으면 False를 반환한다.
     """
     return harvested_at is not None
+
+
+# growth_level을 결정하는 함수
+def calculate_growth_level(plant_type, planted_at):
+    """
+    planted_at으로부터 오늘까지의 날짜를 계산해서
+    발아기(germination), 성장기(growth), 수확기(harvest) 단계를 반환한다.
+    모든 경우가 아닐 경우, 비활성기(nothing)을 반환한다.
+    """
+    today = timezone.now().date()
+    days_since_planted = (today - planted_at.date()).days
+    
+    if plant_type.germination_period_start <= days_since_planted <= plant_type.germination_period_end:
+            return 'germination'
+    elif plant_type.growth_period_start <= days_since_planted <= plant_type.growth_period_end:
+        return 'growth'
+    elif plant_type.harvest_period_start <= days_since_planted <= plant_type.harvest_period_end:
+        return 'harvest'
+    else:
+        return 'nothing'
--- plants/views.py
+++ plants/views.py
@@ -1,5 +1,6 @@
 from .models import *
 from .serializer import *
+from .utils import determine_is_harvested, calculate_growth_level
 
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,6 +19,14 @@ class PlantList(APIView):
     authentication_classes = [JWTAuthentication]
     
     def get(self, request):
-        plants = Plant.objects.all()
-        serializer = PlantSerializer(plants, many=True)
+        plants = Plant.objects.filter(user_id=request.user) # 인증된 사용자의 식물만 확인
+        plant_data = []
+        
+        for plant in plants:
+            plant_info = PlantSerializer(plant).data
+            plant_info['is_harvested'] = determine_is_harvested(plant.harvested_at)
+            plant_info['growth_level'] = calculate_growth_level(plant.plant_type_id, plant.planted_at)
+            plant_data.append(plant_info)
+            
+        return Response(plant_data, status=status.HTTP_200_OK)
         
