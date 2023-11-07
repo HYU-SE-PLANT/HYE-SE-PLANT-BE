7c2913a7e34959ee4801575337b369ffa11cf3b4
OnlyYellow <ash1823@naver.com>
2023-11-07 22:53:37 +0900

    feat/ add plants model

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Plant_Type(models.Model):
+    id = models.IntegerField(primary_key=True, null=False, blank=False)
+    plant_name = models.CharField(max_length=30, null=False, blank=False)
+    plant_temperature = models.CharField(max_length=30, null=False, blank=False)
+    plant_humidity = models.CharField(max_length=30, null=False, blank=False)
+    plant_illuminance = models.CharField(max_length=30, null=False, blank=False)
+    plant_bloom_season = models.CharField(max_length=30, null=False, blank=False)
+    plant_watering_cycle = models.CharField(max_length=30, null=False, blank=False)
+    plant_difficulty = models.CharField(max_length=10, null=False, blank=False)
+    plant_caution = models.CharField(max_length=255, null=False, blank=False)
+    germination_period_start = models.IntegerField(null=False, blank=False)
+    germination_period_end = models.IntegerField(null=False, blank=False)
+    growth_period_start = models.IntegerField(null=False, blank=False)
+    growth_period_end = models.IntegerField(null=False, blank=False)
+    harvest_period_start = models.IntegerField(null=False, blank=False)
+    harvest_period_end = models.IntegerField(null=False, blank=False)
+    
+    def __str__(self):
+        return self.plant_name
+
+
+class Plant(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    plant_nickname = models.CharField(max_length=255, null=False, blank=False)
+    plant_picture_url = models.CharField(max_length=255, null=False, blank=False)
+    plant_type_id = models.ForeignKey(Plant_Type, null=False, blank=False, on_delete=models.CASCADE)
+    planted_at = models.DateTimeField(null=False, blank=False)
+    harvested_at = models.DateTimeField(null=True)
+    is_harvested = models.BooleanField(default=False, null=False, blank=False)
+    user_id = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.plant_nickname
