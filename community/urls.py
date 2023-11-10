ad2282d37918b1c7eea7443280d6f36a6d87a79a
OnlyYellow <ash1823@naver.com>
2023-11-10 14:35:16 +0900

    fix/ community url delete slash

--- community/urls.py
+++ community/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', QuestionList.as_view()),
-    path('create/', QuestionCreate.as_view()),
+    path('create', QuestionCreate.as_view()),
     path('question_detail', QuestionDetail.as_view()),
-    path('comment/', CommentCreate.as_view()),
+    path('comment', CommentCreate.as_view()),
 ]
