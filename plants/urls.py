bd04f26f1000165e818c22cc53407227d5a6f081
OnlyYellow <ash1823@naver.com>
2023-11-10 14:47:50 +0900

    fix/ plants url delete slash

--- plants/urls.py
+++ plants/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', PlantList.as_view()),
-    path('create/', PlantCreate.as_view()),
-    path('type/', PlantType.as_view()),
+    path('create', PlantCreate.as_view()),
+    path('type', PlantType.as_view()),
     path('plant_detail', PlantDetail.as_view()),
 ]
