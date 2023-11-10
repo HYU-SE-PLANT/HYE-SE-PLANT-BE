8bf9c29a57222bace30b09a83062aded9e87c770
OnlyYellow <ash1823@naver.com>
2023-11-10 19:28:52 +0900

    feat/ articles views get list

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import Article
+from .serializer import ArticleSerializer
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework.views import APIView
+
+from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+# Article을 띄워서 보여주기
+class ArticleList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
