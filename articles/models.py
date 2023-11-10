da67c0b700ee386fc37c50dd0e7781ce6d3fcad3
OnlyYellow <ash1823@naver.com>
2023-11-10 19:12:38 +0900

    feat/ articles model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    article_title = models.CharField(max_length=255, null=False, blank=False)
+    article_content_url = models.CharField(max_length=255, null=False, blank=False)
+    article_thumbnail_url = models.CharField(max_length=255, null=False, blank=False)
+    
+    def __str__(self):
+        return self.article_title
