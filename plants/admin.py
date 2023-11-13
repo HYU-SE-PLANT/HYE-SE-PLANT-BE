41dad8a6704b825755e51a3527c45296a82d582e
OnlyYellow <ash1823@naver.com>
2023-11-13 17:48:47 +0900

    fix/ plant disease type admin

--- plants/admin.py
+++ plants/admin.py
@@ -3,7 +3,10 @@ from plants.models import *
 
 
 # Register your models here.
+class PlantDiseaseTypeCheck(admin.ModelAdmin):
+    list_display = ('id', 'plant_disease_name')
+
 admin.site.register(Plant)
 admin.site.register(Plant_Type)
-admin.site.register(Plant_Disease_Type)
+admin.site.register(Plant_Disease_Type, PlantDiseaseTypeCheck)
 admin.site.register(Plant_Disease_Record)
