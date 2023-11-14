11b033a90564e139b674aeb7dc2bb9495236af05
OnlyYellow <ash1823@naver.com>
2023-11-14 12:00:17 +0900

    fix/ 식물 성장 정도 수정

--- plants/admin.py
+++ plants/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from plants.models import *
 from django.utils import timezone
+from .utils import calculate_growth_level
 
 
 # Register your models here.
 class PlantCheck(admin.ModelAdmin):
-    list_display = ('id', 'plant_nickname', 'days_since_planted', 'user_id', 'plant_type_id', 'planted_at', 'harvested_at')
+    list_display = ('id', 'plant_nickname', 'days_since_planted', 'growth_level', 'user_id', 'plant_type_id', 'planted_at', 'harvested_at')
     
     def days_since_planted(self, obj):
         today = timezone.now().date()
-        return (today - obj.planted_at.date()).days + 1
+        return (today - obj.planted_at.date()).days
+    
+    def growth_level(self, obj):
+        return calculate_growth_level(obj.plant_type_id, obj.planted_at)
 
 
 class PlantDiseaseTypeCheck(admin.ModelAdmin):
