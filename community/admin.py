55ddd706b90e61455316fb8ffbb3dc64f4051275
OnlyYellow <ash1823@naver.com>
2023-11-24 23:31:43 +0900

    feat/ add CommentCheck to see in admin page

--- community/admin.py
+++ community/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from community.models import Question
+from community.models import Question, Comment
 
 
-# Register your models here.
 class QuestionCheck(admin.ModelAdmin):
     list_display = ('id', 'user' , 'question_title', 'created_at')
     search_fields = ('user', 'question_title')
     ordering = ('-created_at',)
 
+
+class CommentCheck(admin.ModelAdmin):
+    list_display = ('id', 'question', 'comment_content', 'created_at')
+    search_fields = ('question', 'comment_content')
+    ordering = ('-created_at',)
+
 admin.site.register(Question, QuestionCheck)
+admin.site.register(Comment, CommentCheck)
