5fe96d8d17f38d5fdcba000cb8a9a198ba31898b
OnlyYellow <ash1823@naver.com>
2023-11-25 00:06:31 +0900

    fix/ checking if there is comment function

--- community/utils.py
+++ community/utils.py
@@ -1,3 +1,10 @@
+from .models import Comment
+
+
 # 질문에 댓글이 존재하는지 판단 - 반환값: true/false
 def get_question_is_answered(question):
-    return hasattr(question, 'comment')
+    try:
+        Comment.objects.get(question=question)
+        return True
+    except Comment.DoesNotExist:
+        return False
