f9b7790d4b076f0fcc3f50c72c76c6854246747d
OnlyYellow <ash1823@naver.com>
2023-11-24 22:32:24 +0900

    fix/ change serializer to allow just one comment

--- community/serializer.py
+++ community/serializer.py
@@ -16,9 +16,9 @@ class CommentSerializer(serializers.ModelSerializer):
 class QuestionSerializer(serializers.ModelSerializer):
     # account_id 받아올 것
     user = serializers.ReadOnlyField(source = 'user.account_id')
-    comments = CommentSerializer(many=True, read_only=True)
+    comment = CommentSerializer(read_only=True)
     
     class Meta:
         model = Question
         # 표시되는 항목
-        fields = ['id', 'question_title', 'created_at', 'user', 'question_content', 'comments']
+        fields = ['id', 'question_title', 'created_at', 'user', 'question_content', 'comment']
